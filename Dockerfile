c104bd1e0b105fe21a8e47c5ba1186f1e288798a
Colin Simmons <colin.simmons@engineerbetter.com>
2017-11-06 19:54:42 +0000

    add a verify_image step

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,7 @@ RUN mkdir -p /root/.terraform.d/providers/linux_amd64/
 COPY terraform-provider-cloudfoundry /root/.terraform.d/providers/linux_amd64/terraform-provider-cloudfoundry
 
 ADD .terraformrc /root/
+
+# Verify image
+COPY verify_image.sh /tmp/verify_image.sh
+RUN /tmp/verify_image.sh && rm /tmp/verify_image.sh
---
+++ verify_image.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo "Checking that correct config files have been placed"
+stat -t ~/.terraformrc
+stat -t ~/.terraform.d/providers/linux_amd64/terraform-provider-cloudfoundry-*
+
+mkdir -p tmp-test-dir
+cd tmp-test-dir || exit
+cat << EOF > config.tf
+provider "cloudfoundry" {
+  api_endpoint = "https://api.local.pcfdev.io"
+  username = "admin"
+  password = "admin"
+  skip_ssl_validation = true
+}
+
+data "cloudfoundry_organization" "pcfdev" {
+  name = "pcfdev-org"
+}
+
+data "cloudfoundry_space" "pcfdev" {
+    name = "pcfdev-space"
+    org_id = "${data.cloudfoundry_organization.pcfdev.id}"
+}
+
+resource "cloudfoundry_service" "uaa-db" {
+  name = "uaa-db"
+  space_id = "${data.cloudfoundry_space.pcfdev.id}"
+  service = "p-mysql"
+  plan = "512mb"
+}
+EOF
+
+echo "Checking that terraform init succeeds"
+terraform init
+
+cd .. && rm -rf tmp-test-dir
